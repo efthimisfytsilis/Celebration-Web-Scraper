ae6e65b8523cda0ef6b47e9ef58fac426b14d0cb
Efthimis Fytsilis <efthimisfytsi@gmail.com>
2024-02-29 14:15:29 +0200

    fixed editing bugs

--- app.py
+++ app.py
@@ -9,16 +9,21 @@ class myTreeView(ttk.Treeview):
     def __init__(self, master, df, **kwargs):
         super().__init__(master, **kwargs)
         self.df = df
+        self.row_identifier_to_index = []
         self.bind("<Double-1>", self.on_double_click)
-    
+
     def on_double_click(self, event):
 
         region_clicked = self.identify("region", event.x, event.y)
+        print(f'You clicked on {region_clicked}')
         if region_clicked == "cell":
-            column = self.identify_column(event.x)   
+            column = self.identify_column(event.x)
+            print(f'You clicked on column {column}')   
             column_index = int(column.replace('#', ''))         
             item = self.identify("item", event.x, event.y)
+            print(f'You clicked on item {item}')
             selected_item = self.item(item, "values")[column_index-1]
+            print(f'You selected {selected_item}')
             
             # Create an entry widget and place it in the cell selected
             box = self.bbox(item, column)
@@ -42,6 +47,8 @@ class myTreeView(ttk.Treeview):
         event.widget.destroy()
     
     def on_enter_key(self, event):
+        self.row_identifier_to_index = self.df.index.tolist()
+        print(self.row_identifier_to_index)
         entry = event.widget
         new_value = entry.get()
         column = entry.editing_column_index - 1
@@ -49,11 +56,12 @@ class myTreeView(ttk.Treeview):
         self.set(item, column, new_value)
         
         # Update the dataframe
-        row_index = int(item[1:]) - 1  # Convert item ID to row index
-        column_name = self.heading(column)["text"]
+        row_index = self.row_identifier_to_index[int(item)]
+        # row_index = int(item[1:]) - 1  # Convert item ID to row index
+        column_name = self.heading(column)["text"].lower()
         # print(f'Row index: {row_index}, Column name: {column_name}, New value: {new_value}')
         # print(self.df)
-        self.df.at[row_index, column_name.lower()] = new_value  # Update value in the DataFrame
+        self.df.at[row_index, column_name] = new_value  # Update value in the DataFrame
         entry.destroy()
         print(self.df)
 
@@ -85,11 +93,12 @@ def search_name(event=None):
         tree.delete(*tree.get_children())  # Clear the treeview
         for row in db.itertuples():
             if search_query in row.name:
-                tree.insert('', 'end', values=row[1:])
+                print(row)
+                tree.insert('', 'end', values=row[1:], iid=row[0])
     else:
         tree.delete(*tree.get_children())
         for row in db.itertuples():
-            tree.insert('', 'end', values=row[1:])
+            tree.insert('', 'end', values=row[1:], iid=row[0])
     search_entry.delete(0, 'end')  # Clear the search entry
 
 
@@ -118,7 +127,7 @@ if __name__ == '__main__':
         tree.column(col, anchor="center")
 
     for row in db.itertuples():
-        tree.insert('', 'end', values=row[1:]) # row[0] is the index
+        tree.insert('', 'end', values=row[1:], iid=row[0]) # row[0] is the index
 
     tree.grid(row=1, column=0, padx=10, sticky="nsew")
     
